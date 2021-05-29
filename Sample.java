fa5f547d3c337d8a2d0b9ef64cc5a2920e696b15
mdfazal <fazalmd964@gmail.com>
2021-05-29 16:45:19 +0530

    Learning Strings

--- Sample.java
+++ Sample.java
@@ -1,10 +1,24 @@
 public class Sample {
     public static void main (String[] args) throws IOException
     {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
         char chr;
         System.out.println("Enter a character to check the case of a letter or digit ");
-        char = (char)(br.read());
-        
+        chr = (char)(br.read());
+        if(Character.isLetter(chr))
+        {
+            System.out.println(chr+" is a letter");
+            if(Character.isUpperCase(chr)==true)
+            System.out.println(chr+" is a Upper case letter");
+            if(Character.isLowerCase(chr)==true)
+            System.out.println(chr+" is a lower case letter");
+        }
+        else
+        {
+            if(Character.isDigit(chr)==true)
+            System.out.println(chr+" is a digit");
+            else
+            System.out.println(chr+ " is a special character");
+        }
     }
 }
