846f1dff14e9ab816038c9729cbee253916d70c9
mdfazal <fazalmd964@gmail.com>
2021-05-29 16:39:01 +0530

    Learning Strings

---
+++ Sample.java
@@ -0,0 +1,10 @@
+public class Sample {
+    public static void main (String[] args) throws IOException
+    {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char chr;
+        System.out.println("Enter a character to check the case of a letter or digit ");
+        char = (char)(br.read());
+        
+    }
+}
